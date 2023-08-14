48915ed69dff7811130f56d9c00d4e2ec25634ba
krsk-m2 <shatenu007@gmail.com>
2023-08-14 23:24:01 +0900

    changeContents0814

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.onload = (event) => {
 const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+myHeading.textContent = "main title";
 
 document.querySelector("h1").addEventListener("click", function () {
     alert("痛っ! つつかないで!");
